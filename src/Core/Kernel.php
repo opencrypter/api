6c55c7229994c1ba758d918f149a81177d69d795
Daniel <dan@dfu.io>
2019-01-20 10:08:42 +0100

    Services unified in a single file spec

--- src/Core/Kernel.php
+++ src/Core/Kernel.php
@@ -36,6 +36,8 @@ class Kernel extends BaseKernel
         $loader->load($confDir.'/{packages}/'.$this->environment.'/**/*'.self::CONFIG_EXTS, 'glob');
         $loader->load($confDir.'/{services}/*'.self::CONFIG_EXTS, 'glob');
         $loader->load($confDir.'/{services}/'.$this->environment.'/**/*'.self::CONFIG_EXTS, 'glob');
+        $loader->load($confDir.'/{services}'.self::CONFIG_EXTS, 'glob');
+        $loader->load($confDir.'/{services}_'.$this->environment.self::CONFIG_EXTS, 'glob');
     }
 
     protected function configureRoutes(RouteCollectionBuilder $routes)
